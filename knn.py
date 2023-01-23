495d7f37fb84a93c9490a8bc9556c4d673208e92
ArekMalek <40797527+ArekMalek@users.noreply.github.com>
2023-01-23 23:38:02 +0100

    Update knn.py

--- knn.py
+++ knn.py
@@ -61,39 +61,3 @@ class KNN:
             return sorted_distances_labels
         else:
             return distances_labels
-            else:
-                labels_amount.update({label: x + 1})
-        return max(labels_amount, key=lambda k: labels_amount[k])
-
-    def mergeSort(self, values, labels):
-        if len(values) > 1:
-            r = len(values) // 2
-            L = values[:r]
-            M = values[r:]
-            r2 = len(labels) // 2
-            L2 = labels[:r2]
-            M2 = labels[r2:]
-            self.mergeSort(L, L2)
-            self.mergeSort(M, M2)
-            i = j = k = 0
-            while i < len(L) and j < len(M):
-                if L[i] < M[j]:
-                    values[k] = L[i]
-                    labels[k] = L2[i]
-                    i += 1
-                else:
-                    values[k] = M[j]
-                    labels[k] = M2[j]
-                    j += 1
-                k += 1
-            while i < len(L) and i < len(L2):
-                values[k] = L[i]
-                labels[k] = L2[i]
-                i += 1
-                k += 1
-            while j < len(M) and j < len(M2):
-                values[k] = M[j]
-                labels[k] = M2[j]
-                j += 1
-                k += 1
-        return labels
